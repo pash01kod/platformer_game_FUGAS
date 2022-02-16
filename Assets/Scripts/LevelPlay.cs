3fb6260d93a38e1268c767175f218dfb794dd7cc
Pablo <paulkr911@gmail.com>
2022-02-17 00:57:34 +0200

    added Main menu and Level menu
    + changing levels

---
+++ Assets/Scripts/LevelPlay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelPlay : MonoBehaviour
+{
+    public void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+        Time.timeScale = 1.0f;
+
+    }
+
+    public void CloseButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void Levels()
+    {
+        SceneManager.LoadScene("LevelMenu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
